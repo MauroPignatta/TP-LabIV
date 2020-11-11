6669f15c09c77c94201ec56e29dab7b26f272697
Mauro Pignatta <mauroj.pignatta@gmail.com>
2020-11-11 15:38:14 -0300

    nueva rama para el service

--- src/edu/labIV/cfg/ConfigSection.java
+++
@@ -1,9 +0,0 @@
-package edu.labIV.cfg;
-
-public enum ConfigSection {
-    DATABASE,
-    MAIL,
-    ENCRYPTOR,
-    ACCOUNT,
-    POST
-}
--- src/edu/labIV/entity/UserStatus.java
+++
@@ -1,6 +0,0 @@
-package edu.labIV.entity;
-
-public class UserStatus {
-    public final static String ONLINE = "Conectado";
-    public final static String OFFLINE = "Desconectado";
-}
