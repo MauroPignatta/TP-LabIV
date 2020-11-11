40f29ff3fef4cdbcd7c237884163c71e1d656b5a
Mauro Pignatta <mauroj.pignatta@gmail.com>
2020-11-11 16:31:47 -0300

    Cambios al archivo de config.

---
+++ src/edu/labIV/mail/ActivationMail.java
@@ -0,0 +1,25 @@
+package edu.labIV.mail;
+
+public class ActivationMail extends Mail{
+
+    private static final String subject = "Devs Social Network - Cuenta Activada";
+
+    private static boolean isInitialized;
+    private static String htmlContent;
+
+    private static void init(){
+        htmlContent = getHTMLContent("res/mail/activation.html");
+        isInitialized = true;
+    }
+
+    public static String getBody(){
+        if(!isInitialized)
+            init();
+        return htmlContent;
+    }
+
+    public static String getSubject() {
+        return subject;
+    }
+
+}
